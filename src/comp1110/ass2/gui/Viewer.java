294083c3b269a3e5fbe05d16c11ef3669c667ed6
Chunhao Li <u6527752@anu.edu.au>
2019-04-02 09:44:45 +1100

    add main to view

--- src/comp1110/ass2/gui/Viewer.java
+++ src/comp1110/ass2/gui/Viewer.java
@@ -215,4 +215,8 @@ public class Viewer extends Application {
             primaryStage.setScene(scene);
             primaryStage.show();
         }
+
+    public static void main(String[] args) {
+        launch();
+    }
     }
